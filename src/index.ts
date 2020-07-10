3b8595353748a3d5d403513f73ed3a08c5b13ed6
Koen Verburg <creativekoen@gmail.com>
2020-07-10 22:06:59 +0200

    chore: added the main entry file

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { MediumHttpClient } from './client'
+import { Converter } from './converter'
+import fs from 'fs'
+import path from 'path'
+
+// const medium = new Client()
+// MediumHttpClient.getArticle('https://medium.com/javascript-in-plain-english/a-practical-guide-to-become-a-senior-frontend-developer-553ec50e2933')
+
+const jsonContent = fs.readFileSync(path.resolve(__dirname, '..', 'data', 'example.response.json'))
+
+const verter = new Converter(JSON.parse(jsonContent.toString()))
+verter.convert()
