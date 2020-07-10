ebdcea6506d6e2c4f2e3f8cab2ace8c552880b27
Koen Verburg <creativekoen@gmail.com>
2020-07-10 22:05:16 +0200

    test(converter): updated the tests

--- __tests__/converter.spec.ts
+++ __tests__/converter.spec.ts
@@ -1,10 +1,14 @@
 import { Converter } from '../src/converter'
-import testPost from './test_post.md'
+import fs from 'fs'
+import path from 'path'
+
+const markdownFilePath = path.resolve(__dirname, 'test_post.md')
+const result = fs.readFileSync(markdownFilePath, { encoding: 'utf8'})
 
 describe('converter', () => {
   it('should render markdown', () => {
     const converter = new Converter('')
 
-    expect(converter.run()).toEqual(testPost)
+    expect(converter.run()).toEqual(result)
   })
 })
