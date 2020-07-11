e9f57f13154cad7757a92555b882514434d3f5c6
Koen Verburg <creativekoen@gmail.com>
2020-07-11 19:56:38 +0200

    improv(mediaTypes): added mediaResourceMediumPost

--- src/Enums/mediaTypes.ts
+++ src/Enums/mediaTypes.ts
@@ -1,4 +1,5 @@
 export enum mediaTypes {
   MediaResourceExternalLink = 'MediaResourceExternalLink',
-  MediaResourceTweet = 'MediaResourceTweet'
+  MediaResourceTweet = 'MediaResourceTweet',
+  MediaResourceMediumPost = 'MediaResourceMediumPost'
 }
