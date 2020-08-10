971dfb8381ccbacba80bfc1ffa51d2378ecc7bea
Koen Verburg <creativekoen@gmail.com>
2020-08-10 18:10:56 +0200

    refactor(async/sync): updated the main function

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { Converter } from './converter'
 import { MediumHttpClient } from './client'
 import { StorageService } from './Services/StorageService'
+import { Speech } from './Speech'
 
 void (async () => {
   const urls = [
@@ -16,10 +17,17 @@ void (async () => {
 
     const jsonArticle = await httpClient.getArticle(url)
     const converter = new Converter(jsonArticle)
+    const speechConverter = new Speech()
 
     converter.toMarkdownAsync().then(markdown => {
       StorageService.saveMarkdownFile(markdown.filename, markdown.content)
   })
+
+    converter.toRawTextAsync().then(text => {
+      speechConverter.ConvertToAudioFile(text.filename, text.filenameChunks, text.content).then(results => {
+        console.log('results', results);
+      })
+    })
 
   })
 })()
