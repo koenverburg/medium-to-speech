aef80731cacf0c431b07148534c985c6fb417e34
Koen Verburg <creativekoen@gmail.com>
2020-08-25 22:24:12 +0200

    feat(async/sync): cleaned up the filemerge class

--- src/Helpers/FileMerge.ts
+++ src/Helpers/FileMerge.ts
@@ -1,15 +1,9 @@
-import fs from 'fs'
-import path from 'path'
 import { execSync } from 'child_process'
 
 export class FileMerge {
-  public static merge(inputDir: string, output: string) {
+  public static merge(files: string[], output: string) {
+    const mergeCommand = `sox ${files.join(' ')} ${output}`
 
-    const files = fs.readdirSync(
-      path.resolve(inputDir)
-    ).filter(x => x.endsWith('.wav')).join(' ')
-
-    const mergeCommand = `sox ${files} ${output}`
     execSync(mergeCommand)
 
     console.log(`FileMerge: done!`);
