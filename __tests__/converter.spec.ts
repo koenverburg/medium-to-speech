264dc114c801c759936f9db3cf44b2c9d4cd6e0b
Koen Verburg <creativekoen@gmail.com>
2020-09-15 20:15:51 +0200

    test(converter): updated the spec files with two test cases for markdown and for text

--- __tests__/converter.spec.ts
+++ __tests__/converter.spec.ts
@@ -1,14 +1,33 @@
 import { Converter } from '../src/converter'
 import fs from 'fs'
 import path from 'path'
+import mockResponse from './__mock-data__/example.response.json'
 
-const markdownFilePath = path.resolve(__dirname, 'test_post.md')
-const result = fs.readFileSync(markdownFilePath, { encoding: 'utf8'})
+const cat = (filepath: string) =>
+  fs.readFileSync(path.resolve(__dirname, filepath), { encoding: 'utf8' })
+
+const textResult = cat('./__mock-data__/example-text.txt')
+const markdownResult = cat('./__mock-data__/example-article.md')
 
 describe('converter', () => {
-  it('should render markdown', () => {
-    const converter = new Converter('')
+  let converter: Converter;
 
-    expect(converter.run()).toEqual(result)
+  beforeAll(() => {
+    converter = new Converter(mockResponse)
+  })
+
+  it('should render markdown', async () => {
+    const markdown = await converter.toMarkdownAsync()
+
+    expect(markdown.content).toEqual(markdownResult)
+  })
+
+  it('should render text', async () => {
+    const text = await converter.toRawTextAsync()
+    // Don't question this.. I'm getting spaces back somewhere
+    const originalText = text.content.split('\n').map((l: string) => l.trim()).join('\n')
+    const newTestResult = textResult.split('\n').map(l => l.trim()).join('\n')
+
+    expect(originalText).toEqual(newTestResult)
   })
 })
